4a869e341cb2bd532030807b4176b119ffc252da
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2023-10-03 22:54:37 -0400

    Being object orient refactor, in working state

--- data_animation.py
+++ data_animation.py
@@ -1,86 +1,113 @@
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
 import pandas as pd
-
+import math
+import copy
+import os
 from matplotlib.animation import FuncAnimation, writers
 # from mat
 from scipy.signal import savgol_filter
 
-plt.rcParams['animation.ffmpeg_path'] = r"C:\FFMPEG\bin\ffmpeg.exe"
+FRAME_RATE = 60 # fps
+TIME_LENGTH = 20 # seconds
+FRAME_INTERVAL = math.floor(1000 / FRAME_RATE) # ms
+FRAME_LENGTH = FRAME_RATE * TIME_LENGTH # frames
 
+#FIXME: make this read from a config file
+plt.rcParams['animation.ffmpeg_path'] = r"C:\Users\AA\Desktop\FFMPEG\bin\ffmpeg.exe"
+
+#FIXME: make this read from a config file
 # Needs to be configured depending on what headings are given to the data
-m = pd.read_csv("working_directory/2023_01_29-03_09_58_PM.csv")
+m = pd.read_csv("examples/SF6 animation data.csv")
 
-# print(m.keys())
-# ctime = m['Timestamp'][:-250]
-cmass = m['Honeywell S-type (Ox tank)']
-cmass = savgol_filter(cmass, 2500, 2)
-# cmass = 
-cinjector = m['PT-3 Injector']
-cinjector = savgol_filter(cinjector, 2500, 2)
-# c_cc_pressure = m['PT-3 CC']
-cthrust = m['Thrust']
-
-# c_dp = m['dP']
-
+# FIXME: TO BE CLEANED UP AND MODULARIZED
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
-
-ttime = []
-
-
-
-tinjector = []
-toxpressureline, = ax1.plot([], [], label = "PT-3 Injector (psi)", color = 'blue')
-
-# t_ccpressure = []
-# t_ccpressure_line, = ax1.plot([], [], label = "PT-3 CC", color = 'green')
-
-tthrust = []
-tthrust_line, = ax1.plot([], [], label = "Thrust (lbf)", color = 'red')
-
 ax1.set_xlim(0, 20)
 ax1.set_ylim(-100, 3000)
+
+# Timestap as a base refference
+time_refference = m['Timestamp']
+plotted_time = []
+time_cursor = 0 # The index of the current time refference, starts at -1 since no data is plotted
+
+# A time varaible to track how far through the animation we are in seconds, used to determine what data to put into the plots
+time = 0
+
+# Each data item
+class PlotItem:
+    def __init__(self, display_name, csv_colum, unit, color,filtered=True):
+        self.name = display_name
+        self.csv_colum = csv_colum
+        self.unit = unit
+        self.color = color
+
+        self.data = m[csv_colum]
+        if filtered:
+                self.data = savgol_filter(self.data, 10, 2)
+        self.plotted_data = [] # Starts off empty and grows over time
+
+        # Create the empty line object that will be filled out as the animiation runs
+        self.line = ax1.plot([], [], label = f"{display_name} ({unit})", color = color)[0]
+
+    def tick(self): # a tick pushing the data forward one recording frame, and this might happen multiple times per animation frame'
+        self.plotted_data.append(self.data[time_cursor])
+        self.line.set_data(plotted_time, self.plotted_data)
+
+lines = []
+
+tankMass = PlotItem("Omega S-Type - Ox Tank Mass", "Omega S-Type - Ox Tank Mass", "kg", "black")
+ccPressure = PlotItem("PT-3 CC 2", "PT-3 CC", "psi", "orange")
+# ccPressureUnfiltered = PlotItem("PT-3 CC Unfiltered", "PT-3 CC", "psi", "green",filtered=False)
+thrust = PlotItem("Thrust", "Thrust", "lbf", "red")
+
+lines.append(tankMass)
+lines.append(ccPressure)
+# lines.append(ccPressureUnfiltered)
+lines.append(thrust)
+
+#FIXME: make it easy to set which axies which lines are on
+ax1.set_xlim(0, 20)
+ax1.set_ylim(-100, 1000)
 ax1.set_ylabel('psi/lbf')
 
-
-ax2 = ax1.twinx()  
-ax2.set_ylim(0, 100)
-ax2.set_ylabel('kg')
-
-tmass = []
-tmass_line, = ax2.plot([], [], label = "Omega S-Type - Ox Tank Mass (kg)", color = 'black')
+#FIXME: legends for the axies need to be better (x axis)
 
 ax1.legend(loc = 'upper left')
-ax2.legend(loc = 'upper right')
+# ax2.legend(loc = 'upper right')
 
-global time
-time = 0
-tsamp = 0.02
 
-def animate(i):
+def animate(frame_number):
+    global time_cursor
     global time
-    ttime.append(time)
-    time += tsamp
+    time += FRAME_INTERVAL/1000 # time in seconds having a frame interval added to it in ms, so must be adjusted
 
-    tmass.append(cmass[i*100])
-    # t_ccpressure.append(c_cc_pressure[i])
-    tthrust.append(cthrust[i*100])
-    tinjector.append(cinjector[i*100])
-    
-    tmass_line.set_data(ttime, tmass)
-    # t_ccpressure_line.set_data(ttime, t_ccpressure)
-    tthrust_line.set_data(ttime, tthrust)
-    toxpressureline.set_data(ttime, tinjector)
+    while time_refference[time_cursor] <= time: # while the time at the cursor is before what needs to be plotted
+        # Add a recording frame to the plot
+        plotted_time.append(time_refference[time_cursor])
+        
+        for line in lines:
+            line.tick()
+
+        time_cursor += 1
+
+    # each line will be update by the update functions and reflected in the animation
 
 
 # The interval and frames have to add up to the fps set later for the video to be real-time accurate
-ani = animation.FuncAnimation(fig, animate, interval = 20, frames = 1000, repeat = False) 
 
-f = r"working_directory/sf7_animation.mp4"
-Writer = animation.writers['ffmpeg']
-writer = Writer(fps=50, metadata=dict(artist='Waterloo Rocketry Team'))
-ani.save(f, writer=writer) # Comment out this line to get a 'preview' via the matplotlib plot
+# Get the current working directory
+cwd = os.getcwd()
+file = f"{cwd}/working_directory/test.mp4"
+writer = animation.FFMpegWriter(fps=FRAME_RATE,metadata=dict(artist='Waterloo Rocketry Team')) 
+ani = animation.FuncAnimation(fig, animate, interval = FRAME_INTERVAL, frames = FRAME_LENGTH, repeat = False) 
+print(f"Saving video, this will take {TIME_LENGTH}s...")
+ani.save(file, writer=writer) # Comment out this line to get a 'preview' via the matplotlib plot
 
-plt.show()
+# plt.show()
 print('Video saved sucessfully')
+
+
+# The method for this timing to work is that the animation runs at a certain framerate (converted to a time interval), and a time length
+# For each frame, it'll step up the running time variable by the time interval
+# It must then add all the data who's recorded timestamp is smaller than or equal to the running time variable
