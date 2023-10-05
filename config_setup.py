4997dfdab0143525864e5a75c9e0b67af7a018a9
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2023-10-04 21:52:32 -0400

    Delete config_setup.py

--- config_setup.py
+++
@@ -1,3 +0,0 @@
-# Setup config
-FFMPEG_PATH = r"/Users/aa/Documents/Dev/data-video-visualization/ffmpeg" # This should to be to executable file for ffmpeg
-DATA_WORKING_DIR = "working_directory" # where data files will be read from, and videos ouput to
